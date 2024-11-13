599fbae948733282aa216036f5b6b36a7e6cc1f9
sfatew <130330044+sfatew@users.noreply.github.com>
2024-11-13 14:46:57 +0700

    ini_clean

---
+++ Data preprocessing/fix_gameid.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+path = '../Data/RAW/FBREF_Dataset/ligue-1_2023-2024_fixture_data.csv'
+df = pd.read_csv(path)
+df['game_id'] = range(len(df))
+
+# print(df)
+
+df.to_csv(path, index=False)
